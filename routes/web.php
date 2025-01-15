234503e67e12f8541ef0237781e800c6a6a9145b
AbdulMa1505 <is-amsackey3722@st.umat.edu.gh>
2025-01-15 06:16:12 +0000

    resolved the conflict

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
-<<<<<<< HEAD
+
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
-=======
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
->>>>>>> 22adb1fa2969f5ab9ccb162031ac5011abf2ffd6
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
+
 Route::resource('student',StudentController::class);
-=======
-Route::resource('category',CategoryController::class);
->>>>>>> 22adb1fa2969f5ab9ccb162031ac5011abf2ffd6
+
+
